938a450b332b0b15ae1df786a17ec1df8686365f
Ignacio Millan <nachomillangarcia@gmail.com>
2026-03-12 14:52:05 +0100

    Fix dockerfile

--- src/main/java/com/example/javaServer.java
+++ src/main/java/com/example/javaServer.java
@@ -47,7 +47,7 @@ public class javaServer {
                     req.ip());
         });
 
-        get("/", (req, res) -> "Hello from Java inside Docker! Tekton 21");
+        get("/", (req, res) -> "Hello from Java inside Docker! Tekton 121");
         get("/health", (req, res) -> "OK");
         get("/home", (req, res) -> {
             try {
